c7aa77b0fa6b67dc98cfe24ef2b4705c42f564ab
Shawn Taylor <shawntaylor@weecology.org>
2020-03-02 14:06:15 -0700

    dash map and timeseries working with model data
    with the subset of NE CO data used  in the ESA abstract
    
    working with:
    -selecting a decade
    -selecting a pixel
    -showing the yearly average of that pixel
     with two scenarios!

--- dash_test.py
+++ dash_test.py
@@ -6,6 +6,8 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 
+from textwrap import dedent as d
+
 import numpy as np
 import geopandas as gp
 import json
@@ -16,16 +18,21 @@ with open('usa_latlon_grid.geojson','r') as f:
 n_features = len(us_grid['features'])
 [f.update(id=i) for i,f in enumerate(us_grid['features'])]
 
-latlon_df = pd.read_csv('data/downscaled_test_data.csv')
+latlon_df = pd.read_csv('data/NE_CO_phenograss.csv')
 pixel_ids = latlon_df[['latitude','longitude']].drop_duplicates().reset_index()
 pixel_ids['pixel_id'] = pixel_ids.index
 
 
-latlon_df['year'] = pd.DatetimeIndex(latlon_df.time).year
-latlon_df = latlon_df.groupby(['year','latitude','longitude']).tasmax.mean().reset_index()
+#latlon_df['year'] = pd.DatetimeIndex(latlon_df.time).year
+#latlon_df = latlon_df.groupby(['year','latitude','longitude']).tasmax.mean().reset_index()
 latlon_df = pd.merge(latlon_df ,pixel_ids, on=['latitude','longitude'], how='left')
 
-selectable_years = latlon_df.year.drop_duplicates()
+selectable_years = latlon_df.decade.drop_duplicates()
+selectable_scenarios = latlon_df.scenario.drop_duplicates().to_list()
+
+map_color = latlon_df.groupby(['decade','pixel_id']).phenograss_prediction.mean().reset_index()
+
+
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -58,16 +65,66 @@ app.layout = html.Div([html.Div([html.H1("Grassland Productivity")],
                                       children=''),
                                dcc.Graph(id='map')
                                ]
+                           ),
+                       
+                       html.Div(
+                           id='timeseries-container',
+                           children = [
+                               html.P(id='timeseries-title',
+                                      children=''),
+                               dcc.Graph(id='timeseries')
+                               ]
+                           ),
+                       
+                       html.Div([
+                           dcc.Markdown(d('''
+                             Map click output             
+                           
+                           ''')),
+                           html.Pre(id='click-output')
+                           ]
                            )
-                       ])
+                       ], style={'align-items':'center',
+                                 'justify-content':'center',
+                                 'width':'50%'})
 
+
+@app.callback(
+    dash.dependencies.Output('click-output', 'children'),
+    [dash.dependencies.Input('map', 'clickData')])
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)
+
+
+@app.callback(
+    dash.dependencies.Output('timeseries', 'figure'),
+    [dash.dependencies.Input('map', 'clickData'),
+     dash.dependencies.Input("decade-slider", "value")])
+def update_timeseries(clickData, decade):
+    print(clickData)
+    selected_pixel = clickData['points'][0]['location']
+    print(selected_pixel)
+    pixel_data = latlon_df[(latlon_df.pixel_id==selected_pixel) & (latlon_df.decade==decade)]
+    
+    traces = []
+    for scenario in selectable_scenarios:
+        scenario_data = pixel_data[pixel_data.scenario==scenario]
+        traces.append(go.Scatter(x=scenario_data.doy, y=scenario_data.phenograss_prediction,
+                                 mode='lines+markers',
+                                 name=scenario))
+    
+    
+    #trace = go.Scatter(x=pixel_data.year, y=pixel_data.tasmax)
+    return {'data': traces,
+             "layout": go.Layout(title='Yearly fCover',height=500,width=900)}
 
 @app.callback(
     dash.dependencies.Output("map", "figure"),
     [dash.dependencies.Input("decade-slider", "value")]
 )
 def update_figure(decade):
-    dff = latlon_df[latlon_df.year==decade]
+    dff = map_color[map_color.decade==decade]
+    
 
     # trace = go.Choropleth(geojson=us_grid, locations=dff['id'], z=dff['tasmax'],
     #                       locationmode='geojson-id',
@@ -78,7 +135,7 @@ def update_figure(decade):
     #                                 'title': {"text": 'temp', "side": "bottom"}})
     trace = go.Choropleth(
                     geojson=us_grid,
-                    z = dff['tasmax'],
+                    z = dff['phenograss_prediction'],
                     locations = dff['pixel_id'],
                     featureidkey='id',
                     locationmode='geojson-id'
@@ -86,7 +143,7 @@ def update_figure(decade):
     
     #return {'data':[trace]}
     return {"data": [trace],
-             "layout": go.Layout(title='layout title',height=800,geo_scope='usa',geo={'showframe': False,'showcoastlines': False})}
+             "layout": go.Layout(title='layout title',height=500,width=700,geo_scope='usa',geo={'showframe': False,'showcoastlines': False})}
 
 if __name__ == '__main__':
     app.run_server(debug=True)

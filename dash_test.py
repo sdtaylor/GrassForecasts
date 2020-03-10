54102ce65925d98fb35c983f8b983c5be527ef8e
Shawn Taylor <shawntaylor@weecology.org>
2020-03-10 14:53:08 -0600

    cleaner site layout

--- dash_test.py
+++ dash_test.py
@@ -33,19 +33,17 @@ selectable_scenarios = latlon_df.scenario.drop_duplicates().to_list()
 map_color = latlon_df.groupby(['decade','pixel_id']).phenograss_prediction.mean().reset_index()
 
 
-
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([html.Div([html.H1("Grassland Productivity")],
-                                style={'textAlign': "center", "padding-bottom": "30"}),
-                       
-                       html.Div(id='slider-container',
+page_title_text = html.Div([html.H1("Grassland Productivity Long Term Forecast")],
+                                style={'textAlign': "center", "padding-bottom": "30"})
+
+year_slider_container = html.Div(id='year-slider-container',
                                 children = [
                                     html.P(
-                                        id='slider-text',
+                                        id='year-slider-text',
                                         children='Drag the slider the the descired decade',
                                         ),
                                     dcc.Slider(
@@ -56,27 +54,49 @@ app.layout = html.Div([html.Div([html.H1("Grassland Productivity")],
                                         marks={
                                             year: {'label':str(year)}
                                              for year in selectable_years}
-                                        )]),
-                       
-                       html.Div(
-                           id='map-container',
+                                        )], style={'width':'50%'})
+
+scenario_radio_container = html.Div(id='scenario-radio-container',
+                                    children = [
+                                        html.P(
+                                            id='scenario-radio-text',
+                                            children = 'Select a scenario'
+                                            ),
+                                        dcc.RadioItems(
+                                            options = [{'label':s,'value':s} for s in latlon_df.scenario.unique()],
+                                            value  = latlon_df.scenario.unique()[0]
+                                            )
+                                        ])
+
+response_radio_container = html.Div(id='response-radio-container',
+                                    children = [
+                                        html.P(
+                                            id='response-radio-text',
+                                            children = 'Select a variable to forecast'
+                                            ),
+                                        dcc.RadioItems(
+                                            options = [{'label':'Change in Annual Productivity','value':'change_in_productivity'}],
+                                            value  = 'change_in_productivity'
+                                            )
+                                        ])
+
+map_container = html.Div(id='map-container',
                            children = [
                                html.P(id='map-title',
                                       children=''),
                                dcc.Graph(id='map')
                                ]
-                           ),
-                       
-                       html.Div(
-                           id='timeseries-container',
+                           )
+
+timeseries_container = html.Div(id='timeseries-container',
                            children = [
                                html.P(id='timeseries-title',
                                       children=''),
                                dcc.Graph(id='timeseries')
                                ]
-                           ),
-                       
-                       html.Div([
+                           )
+
+markdown_container = html.Div([
                            dcc.Markdown(d('''
                              Map click output             
                            
@@ -84,11 +104,34 @@ app.layout = html.Div([html.Div([html.H1("Grassland Productivity")],
                            html.Pre(id='click-output')
                            ]
                            )
-                       ], style={'align-items':'center',
-                                 'justify-content':'center',
-                                 'width':'50%'})
+                                          
 
-
+app.layout = html.Div(id='page-container',
+                      children=[
+                          
+                          page_title_text,
+                          
+                          # combo of column counts to make the scenario on
+                          # the left, with reponse/year stacked on right
+                          html.Div(id='selection-container',
+                                   children = [
+                                       scenario_radio_container,
+                                       html.Div([response_radio_container,
+                                                 year_slider_container],
+                                                style={'columnCount':1})                                       
+                                       ], style={'columnCount':2}),
+                          
+                          html.Div(id='figure-container',
+                                   children = [
+                                       map_container,
+                                       timeseries_container
+                                       ],style={'columnCount':2}),
+                          
+                          markdown_container
+                          ], style={'align-items':'center',
+                                    'justify-content':'center'})
+                                 
+                                 
 @app.callback(
     dash.dependencies.Output('click-output', 'children'),
     [dash.dependencies.Input('map', 'clickData')])
@@ -125,14 +168,6 @@ def update_timeseries(clickData, decade):
 def update_figure(decade):
     dff = map_color[map_color.decade==decade]
     
-
-    # trace = go.Choropleth(geojson=us_grid, locations=dff['id'], z=dff['tasmax'],
-    #                       locationmode='geojson-id',
-    #                       featureidkey='id',
-    #                       autocolorscale=False, 
-    #                       colorscale="YlGnBu",marker={'line': {'color': 'rgb(180,180,180)','width': 0.5}},
-    #                       colorbar={"thickness": 10,"len": 0.3,"x": 0.9,"y": 0.7,
-    #                                 'title': {"text": 'temp', "side": "bottom"}})
     trace = go.Choropleth(
                     geojson=us_grid,
                     z = dff['phenograss_prediction'],

3fa9bb173b5b88fc42cdae8fc5a920c76fde8801
Shawn <shawntaylor@weecology.org>
2020-06-25 11:54:30 -0600

    clarify the buffer as resolution

--- tools/etc.py
+++ tools/etc.py
@@ -3,23 +3,25 @@ from geopandas import GeoDataFrame, GeoSeries
 from shapely.geometry import Polygon
 
 
-def build_geojson_grid(reference_xarray, geojson_file=None, polygon_buffer=0.999):
+def build_geojson_grid(reference_xarray, polygon_resolution, geojson_file=None):
     """
     Make a geojson of a grid (of polygons) with the extent and resolution of 
     reference_xarray, an xarray dataset/dataArray with latitude/longitude coordinates. 
-    polygon_buffer is the  spacing between grid polygons.
+    polygon_resolution is the size of the grid polygons.
+    polygon_resolution gets 0.001 subtracted so the polygons dont overlap
     
     Will save as geojson file specified by filename, if not then it will
     return a geopandas geodataframe.
     """
+    polygon_resolution -= 0.001
     full_grid = []
     grid_names = []
     for i, row in reference_xarray[['latitude','longitude']].drop_duplicates().iterrows():
         lat, lon = row['latitude'], row['longitude']
         ur = (lon, lat)      # [-125,28]
-        ul = (lon - polygon_buffer, lat)   # [-126,28]
-        lr = (lon, lat + polygon_buffer)   # [-125,27]
-        ll = (lon - polygon_buffer, lat + polygon_buffer) # [-126,27]
+        ul = (lon - polygon_resolution, lat)   # [-126,28]
+        lr = (lon, lat + polygon_resolution)   # [-125,27]
+        ll = (lon - polygon_resolution, lat + polygon_resolution) # [-126,27]
                
         full_grid.append(Polygon([ur,ul,ll,lr]))
         grid_names.append({'latitude':lat,'longitude':lon})

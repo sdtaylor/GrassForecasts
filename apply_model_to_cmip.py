d9f6d153583c854d008dfbab288d86d35486465b
Shawn <shawntaylor@weecology.org>
2020-07-09 11:38:56 -0600

    updates to phenograss/cmip processing
    - new cmip5 info loading
    - now iterate over the phenograss models I built and evaluated instead
    of using only the original one

--- apply_model_to_cmip.py
+++ apply_model_to_cmip.py
@@ -9,37 +9,20 @@ from dask_jobqueue import SLURMCluster
 from dask.distributed import Client
 import dask
 
+from tools import cmip5_file_tools
+
 #################################################
 # Layout all the data
 
 climate_data_folder = 'data/cmip5_nc_files/'
-climate_model_info = [{'climate_model_name':'ccsm4',
-                       'scenario':'rcp26',
-                       'model_file_search_str': '*CCSM4_rcp26*.nc4'},
-                      #{'climate_model_name':'ccsm4',
-                      #'scenario':'rcp45',
-                      #'model_file_search_str': '*CCSM4_rcp45*.nc4'},
-                      
-                      # {'climate_model_name':'csiro',
-                      #  'scenario':'rcp26',
-                      #  'model_file_search_str': '*CSIRO-Mk3-6-0_rcp26*.nc4'},
-                      # {'climate_model_name':'csiro',
-                      #  'scenario':'rcp45',
-                      #  'model_file_search_str': '*CSIRO-Mk3-6-0_rcp45*.nc4'},
-                      
-                      # {'climate_model_name':'gfdl',
-                      #  'scenario':'rcp26',
-                      #  'model_file_search_str': '*GFDL-ESM2G_rcp26*.nc4'},
-                      # {'climate_model_name':'gfdl',
-                      #  'scenario':'rcp45',
-                      #  'model_file_search_str': '*GFDL-ESM2G_rcp45*.nc4'}
-                      ]
-    
+climate_model_info = cmip5_file_tools.get_cmip5_spec(models='all',scenarios='all')
+n_climate_models = len(climate_model_info)
+
 phenograss_encoding = {'fCover':{'zlib':True,
-                              'complevel':4, 
-                              'dtype':'float32', 
-                              'scale_factor':0.001,  
-                              '_FillValue': -9999}}
+                                 'complevel':4, 
+                                 'dtype':'float32', 
+                                 'scale_factor':0.001,  
+                                 '_FillValue': -9999}}
 
 phenograss_output_folder = 'data/phenograss_nc_files/'
 
@@ -47,7 +30,7 @@ phenograss_output_folder = 'data/phenograss_nc_files/'
 # Dask/ceres config stuff stuff
 ceres_workers          = 100 # Number of slumr jobs started
 ceres_cores_per_worker = 1 # number of cores per job
-ceres_mem_per_worker   = '2GB' # memory for each job
+ceres_mem_per_worker   = '4GB' # memory for each job
 ceres_worker_walltime  = '48:00:00' # the walltime for each worker, HH:MM:SS
 ceres_partition        = 'short'    # short: 48 hours, 55 nodes
                                     # medium: 7 days, 25 nodes
@@ -84,24 +67,41 @@ from tools import xarray_tools
 #climate_model_files = 'data/NE_CO_ccsm4.nc4'
 other_var_ds = xr.open_dataset('data/other_variables.nc')
 
-phenograss = GrasslandModels.utils.load_prefit_model('PhenoGrass-original')  
-phenograss.set_internal_method('numpy')  
+# One model for each of the ecoregions. These were determined in this study: https://github.com/sdtaylor/PhenograssReplication
+phenograss_model_files = ['models/ecoregion-vegtype_ETempForests_GR_PhenoGrass_4dac8b702c3241eb.json',  
+                          'models/ecoregion-vegtype_NWForests_GR_PhenoGrass_4dac8b702c3241eb.json',
+                          'models/ecoregion-vegtype_GrPlains_GR_PhenoGrass_4dac8b702c3241eb.json']
+phenograss_models = [GrasslandModels.utils.load_saved_model(f) for f in phenograss_model_files]
+# Need to use numpy, as opposed to cython method, for gridded data.
+_ = [m.set_internal_method('numpy') for m in phenograss_models]
 
 #all_phenograss_output = []
 for ds_i, ds_info in enumerate(climate_model_info):
     model_files = glob(climate_data_folder + ds_info['model_file_search_str'])
+    
+    print('building climate data {i}/{n}'.format(i=ds_i, n=n_climate_models))
     ds = xarray_tools.compile_cmip_model_data(climate_model_name =  ds_info['climate_model_name'], 
                                               scenario =            ds_info['scenario'], 
                                               climate_model_files = model_files, 
                                               other_var_ds =        other_var_ds, 
                                               chunk_sizes =         chunk_sizes)
+    print('loading climate data into memory')
+    ds.load()
     
-    phenograss_ds = xarray_tools.apply_phenograss_dask_wrapper(model = phenograss, ds=ds).compute() 
-    
-    output_file = 'phenograss_file{i}_{m}_{s}.nc4'.format(i=ds_i, m=ds_info['climate_model_name'], s=ds_info['scenario'])
-    
-    phenograss_ds.to_dataset(name='fCover').to_netcdf(phenograss_output_folder + output_file,
-                                                 encoding = phenograss_encoding)
+    for model_i, model in enumerate(phenograss_models):
+        print('applying phenograss model {i}'.format(i=model_i))
+        phenograss_ds = xarray_tools.apply_phenograss_dask_wrapper(model = model, ds=ds).compute() 
+        
+        # model metadata fitting_set looks like: 'ecoregion-vegtype_NWForests_GR'
+        model_info = model.metadata['fitting_set'].split('_')
+        model_ecoregion = model_info[1]
+        model_vegtype = model_info[2]
+        
+        output_file = 'phenograss_file{i}_{m}_{s}_{e}_{v}.nc4'.format(i=ds_i, m=ds_info['climate_model_name'], s=ds_info['scenario'],
+                                                                      e = model_ecoregion, v = model_vegtype)
+        
+        phenograss_ds.to_dataset(name='fCover').to_netcdf(phenograss_output_folder + output_file,
+                                                          encoding = phenograss_encoding)
     
     # The files get pretty big, so clear them out for the next round
     ds = phenograss_ds = None
